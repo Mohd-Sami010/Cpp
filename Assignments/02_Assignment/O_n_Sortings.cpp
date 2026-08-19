609786b2722243b73189082ad86e514051ee94c0
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-08-19 23:58:03 +0530

    Updated Assignment 2

--- Assignments/02_Assignment/O_n_Sortings.cpp
+++ Assignments/02_Assignment/O_n_Sortings.cpp
@@ -1,143 +1,149 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
-
-void CountingSort(int arr[], int n)
+void countingSort(int arr[], int n)
 {
-    // Step 1: Find the maximum value to know the range
     int maxVal = arr[0];
     for (int i = 1; i < n; i++)
-    {
         if (arr[i] > maxVal)
             maxVal = arr[i];
-    }
-    int count[100] = {0};
+
+    int *count = new int[maxVal + 1];
+    for (int i = 0; i <= maxVal; i++)
+        count[i] = 0;
+
     for (int i = 0; i < n; i++)
         count[arr[i]]++;
 
     int index = 0;
-    for (int num = 0; num <= maxVal; num++)
+    for (int i = 0; i <= maxVal; i++)
     {
-        while (count[num] > 0)
+        while (count[i] > 0)
         {
-            arr[index] = num;
+            arr[index] = i;
             index++;
-            count[num]--;
+            count[i]--;
         }
     }
+
+    delete[] count;
 }
-void BucketSort(int arr[], int n)
+int getMax(int arr[], int n)
 {
     int maxVal = arr[0];
     for (int i = 1; i < n; i++)
-    {
         if (arr[i] > maxVal)
             maxVal = arr[i];
-    }
-
-    int numBuckets = 10;
-    int bucketSize = (maxVal / numBuckets) + 1;
-
-    int buckets[10][20];       // 10 buckets, each can hold up to 20 numbers
-    int bucketCount[10] = {0}; // how many numbers are currently in each bucket
-
-    // Step 1: Place each number into the correct bucket
-    for (int i = 0; i < n; i++)
-    {
-        int bucketIndex = arr[i] / bucketSize;
-        buckets[bucketIndex][bucketCount[bucketIndex]] = arr[i];
-        bucketCount[bucketIndex]++;
-    }
-
-    // Step 2: Sort each bucket using simple insertion sort (small buckets, so still fast)
-    for (int b = 0; b < numBuckets; b++)
-    {
-        for (int i = 1; i < bucketCount[b]; i++)
-        {
-            int key = buckets[b][i];
-            int j = i - 1;
-            while (j >= 0 && buckets[b][j] > key)
-            {
-                buckets[b][j + 1] = buckets[b][j];
-                j--;
-            }
-            buckets[b][j + 1] = key;
-        }
-    }
-
-    // Step 3: Combine all buckets back into the original array
-    int index = 0;
-    for (int b = 0; b < numBuckets; b++)
-    {
-        for (int i = 0; i < bucketCount[b]; i++)
-        {
-            arr[index] = buckets[b][i];
-            index++;
-        }
-    }
+    return maxVal;
 }
-void CountSortByDigit(int arr[], int n, int place)
+
+void countSortForRadix(int arr[], int n, int exp)
 {
-    int output[20];
+    int *output = new int[n];
     int count[10] = {0};
 
-    // Count occurrences of each digit (0-9) at this place value
     for (int i = 0; i < n; i++)
-    {
-        int digit = (arr[i] / place) % 10;
-        count[digit]++;
-    }
+        count[(arr[i] / exp) % 10]++;
 
-    // Change count[i] so it contains the actual position of this digit in output[]
     for (int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    // Build the output array (go backwards to keep it stable)
     for (int i = n - 1; i >= 0; i--)
     {
-        int digit = (arr[i] / place) % 10;
+        int digit = (arr[i] / exp) % 10;
         output[count[digit] - 1] = arr[i];
         count[digit]--;
     }
 
     for (int i = 0; i < n; i++)
         arr[i] = output[i];
+
+    delete[] output;
 }
 
-void RadixSort(int arr[], int n)
+void radixSort(int arr[], int n)
 {
-    // Find the maximum number to know the number of digits
-    int maxVal = arr[0];
+    int maxVal = getMax(arr, n);
+
+    for (int exp = 1; maxVal / exp > 0; exp *= 10)
+        countSortForRadix(arr, n, exp);
+}
+void insertionSortForBucket(int arr[], int n)
+{
     for (int i = 1; i < n; i++)
     {
-        if (arr[i] > maxVal)
-            maxVal = arr[i];
-    }
-
-    // Do counting sort for every digit place (1s, 10s, 100s, ...)
-    for (int place = 1; maxVal / place > 0; place *= 10)
-    {
-        CountSortByDigit(arr, n, place);
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
     }
 }
-void PrintArray(int arr[], int n)
+
+void bucketSort(int arr[], int n, int maxValue)
 {
-    cout << "[";
+    int bucketCount = n;
+    int **buckets = new int *[bucketCount];
+    int *bucketSize = new int[bucketCount];
+
+    for (int i = 0; i < bucketCount; i++)
+    {
+        buckets[i] = new int[n];
+        bucketSize[i] = 0;
+    }
+
     for (int i = 0; i < n; i++)
     {
-        if (i < n - 1)
-            cout << arr[i] << ", ";
-        else
-            cout << arr[i];
+        int bucketIndex = (arr[i] * bucketCount) / (maxValue + 1);
+        buckets[bucketIndex][bucketSize[bucketIndex]] = arr[i];
+        bucketSize[bucketIndex]++;
     }
-    cout << " ]" << endl;
+
+    for (int i = 0; i < bucketCount; i++)
+        insertionSortForBucket(buckets[i], bucketSize[i]);
+
+    int index = 0;
+    for (int i = 0; i < bucketCount; i++)
+    {
+        for (int j = 0; j < bucketSize[i]; j++)
+        {
+            arr[index] = buckets[i][j];
+            index++;
+        }
+    }
+
+    for (int i = 0; i < bucketCount; i++)
+        delete[] buckets[i];
+    delete[] buckets;
+    delete[] bucketSize;
 }
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 int main()
 {
-    int n = 20;
-    int original[20] = {12, 45, 3, 67, 23, 89, 5, 34, 78, 1,
-                        56, 90, 22, 11, 99, 4, 60, 33, 18, 27};
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
 
-    int arr1[20], arr2[20], arr3[20];
+    srand(time(0));
+
+    int *original = new int[n];
+    for (int i = 0; i < n; i++)
+        original[i] = rand() % 10000;
+
+    int *arr1 = new int[n];
+    int *arr2 = new int[n];
+    int *arr3 = new int[n];
+
     for (int i = 0; i < n; i++)
     {
         arr1[i] = original[i];
@@ -145,20 +151,98 @@ int main()
         arr3[i] = original[i];
     }
 
-    cout << "Original array: ";
-    PrintArray(original, n);
+    if (n <= 20)
+    {
+        cout << "\nOriginal Array:\n";
+        printArray(original, n);
+    }
 
-    CountingSort(arr1, n);
-    cout << "\nSorted using Counting Sort: ";
-    PrintArray(arr1, n);
+    // Find max value for bucket sort range
+    int maxValue = original[0];
+    for (int i = 1; i < n; i++)
+        if (original[i] > maxValue)
+            maxValue = original[i];
 
-    BucketSort(arr2, n);
-    cout << "Sorted using Bucket Sort: ";
-    PrintArray(arr2, n);
+    int repetitions = 1;
+    if (n < 1000)
+        repetitions = 100;
+    else if (n < 10000)
+        repetitions = 10;
 
-    RadixSort(arr3, n);
-    cout << "Sorted using Radix Sort: ";
-    PrintArray(arr3, n);
+    // ---------------- Counting Sort ----------------
+    clock_t start1 = clock();
+    for (int r = 0; r < repetitions; r++)
+    {
+        int *temp = new int[n];
+        for (int i = 0; i < n; i++)
+            temp[i] = original[i];
 
+        countingSort(temp, n);
+
+        if (r == repetitions - 1)
+            for (int i = 0; i < n; i++)
+                arr1[i] = temp[i];
+
+        delete[] temp;
+    }
+    clock_t end1 = clock();
+    clock_t ticks1 = end1 - start1;
+
+    // ---------------- Radix Sort ----------------
+    clock_t start2 = clock();
+    for (int r = 0; r < repetitions; r++)
+    {
+        int *temp = new int[n];
+        for (int i = 0; i < n; i++)
+            temp[i] = original[i];
+
+        radixSort(temp, n);
+
+        if (r == repetitions - 1)
+            for (int i = 0; i < n; i++)
+                arr2[i] = temp[i];
+
+        delete[] temp;
+    }
+    clock_t end2 = clock();
+    clock_t ticks2 = end2 - start2;
+
+    // ---------------- Bucket Sort ----------------
+    clock_t start3 = clock();
+    for (int r = 0; r < repetitions; r++)
+    {
+        int *temp = new int[n];
+        for (int i = 0; i < n; i++)
+            temp[i] = original[i];
+
+        bucketSort(temp, n, maxValue);
+
+        if (r == repetitions - 1)
+            for (int i = 0; i < n; i++)
+                arr3[i] = temp[i];
+
+        delete[] temp;
+    }
+    clock_t end3 = clock();
+    clock_t ticks3 = end3 - start3;
+
+    if (n <= 20)
+    {
+        cout << "\nCounting Sort:\n";
+        printArray(arr1, n);
+
+        cout << "\nRadix Sort:\n";
+        printArray(arr2, n);
+
+        cout << "\nBucket Sort:\n";
+        printArray(arr3, n);
+    }
+
+    cout << "\nSORTING ALGORITHM PERFORMANCE\n";
+    cout << "\nAlgorithm\t\tClock Ticks\n";
+    cout << "--------------------------------------------\n";
+    cout << "Counting Sort\t\t" << ticks1 << endl;
+    cout << "Radix Sort\t\t" << ticks2 << endl;
+    cout << "Bucket Sort\t\t" << ticks3 << endl;
     return 0;
 }
